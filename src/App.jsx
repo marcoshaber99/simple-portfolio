835e1bc0e619e4446aac7e010ed9f5ff55106d5e
Marco Haber <marcoshaber99@gmail.com>
2023-03-07 10:18:42 +0200

    finished header

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>    
+    </div>
+  )
+}
+
+export default App
